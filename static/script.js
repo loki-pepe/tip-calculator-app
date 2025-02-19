4ba91ba9ffcc721b36e31c801c27d2d3dee75032
Lovro <lovroperaic@gmail.com>
2025-02-19 18:33:23 +0100

    add reset button functionality and fix rounding issues

--- static/script.js
+++ static/script.js
@@ -1,20 +1,35 @@
 document.addEventListener('DOMContentLoaded', () => {
     const FORM = document.querySelector('#calculator');
-    FORM.addEventListener('submit', handleSubmit);
-    FORM.addEventListener('input', e => handleInput(e, FORM));
     const TIP = document.querySelector('#tip');
     const TOTAL = document.querySelector('#total');
+    const RESET = document.querySelector('#clear');
 
+    FORM.addEventListener('submit', handleSubmit);
+    FORM.addEventListener('input', () => handleInput(FORM));
+    RESET.addEventListener('click', () => {
+        FORM.reset();
+        toggleResetButton(false);
+    })
+
+
+
+    function ceilToFloat(f, decimalDigits) {
+        return Math.ceil(f * 10**decimalDigits) / 10**decimalDigits;
+    }
 
     function getData(form) {
         return Object.fromEntries(new FormData(form));
     }
 
-    function handleInput(e, form) {
+    function handleInput(form) {
         if (!form.checkValidity()) {
+            toggleResetButton(true);
             updateUI(0, 0);
         } else {
             let {bill, tip, people} = parseData(getData(form));
+
+            toggleResetButton(!(isNaN(bill) && isNaN(tip) && isNaN(people)));
+
             let {tipPerPerson, totalPerPerson} = tipCalculator(bill, tip, people);
             updateUI(tipPerPerson, totalPerPerson);
         }
@@ -22,10 +37,6 @@ document.addEventListener('DOMContentLoaded', () => {
 
     function handleSubmit(e) {
         e.preventDefault();
-    }
-
-    function ceilToFloat(f, decimalDigits) {
-        return Math.ceil(f * 10**decimalDigits) / 10**decimalDigits;
     }
 
     function parseData(data) {
@@ -39,14 +50,29 @@ document.addEventListener('DOMContentLoaded', () => {
     function tipCalculator(bill, tipPercent, people) {
         let tip = tipPercent ? bill * tipPercent / 100 : 0;
 
+        if (people === 1) {
+            return {
+                tipPerPerson: tip,
+                totalPerPerson: bill + tip,
+            }
+        }
+
         let tipPerPerson = ceilToFloat(tip / people, 2);
         let totalPerPerson = ceilToFloat((bill + tip) / people, 2);
 
         return {tipPerPerson, totalPerPerson};
     }
 
+    function toggleResetButton(bool) {
+        if (bool) {
+            RESET.removeAttribute('disabled');
+        } else {
+            RESET.setAttribute('disabled', '');
+        }
+    }
+
     function updateUI(tip, total) {
-        TIP.textContent = tip.toFixed(2);
-        TOTAL.textContent = total.toFixed(2);
+        TIP.textContent = isNaN(tip) ? '0.00' : tip.toFixed(2);
+        TOTAL.textContent = isNaN(total) ? '0.00' : total.toFixed(2);
     }
 });
